19e6300f0833527907e618b90681f93f283f72d8
Francisco Javier Jimenez Cohen <francisco.cohen@artekium.com>
2026-01-29 22:39:20 +0100

    add : se agrego la clase alumno

---
+++ src/models/entity/alumno.js
@@ -0,0 +1,10 @@
+export class Alumno {
+    constructor(id, nombre, apellido, edad, curso) {
+        this.id = id;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.edad = edad;
+        this.curso = curso;
+    }
+}
+
